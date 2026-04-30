46b3f334aa011f2658b871c03532123573144455
reiji <tsukiwahigashini0410@gmail.com>
2026-04-30 14:41:33 +0900

    update key shortcut

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,1 +1,9 @@
-vim.keymap.set("i", "jk", "<ESC>", {desc = "Exit insert mode"})
+-- TerminalモードからEscキーでNormalモードに戻れるようにする
+vim.keymap.set('t', '<Esc>', [[<C-\><C-n>]], {noremap = true})
+
+-- ウィンドウ移動を楽にする（Terminalモードから直接移動できるように 't' で設定）
+vim.keymap.set('t', '<C-w><C-h>', [[<C-\><C-n><C-w>h]], {noremap = true})
+vim.keymap.set('t', '<C-w><C-j>', [[<C-\><C-n><C-w>j]], {noremap = true})
+vim.keymap.set('t', '<C-w><C-k>', [[<C-\><C-n><C-w>k]], {noremap = true})
+vim.keymap.set('t', '<C-w><C-l>', [[<C-\><C-n><C-w>l]], {noremap = true})
+
