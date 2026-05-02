677fd80574510d5a2760694bb92d105de52d23a1
lazydune <tsukiwahigashini0410@gmail.com>
2026-05-02 16:48:23 +0900

    add skk mode in terminal

--- lua/plugins/skk.lua
+++ lua/plugins/skk.lua
@@ -7,7 +7,7 @@ return {
             eggLikeNewline = true,
             keepState = false,
         })
-        vim.keymap.set({ "i", "c" }, "<C-j>", "<Plug>(skkeleton-enable)", { silent = true })
+        vim.keymap.set({ "i", "c", "t" }, "<C-j>", "<Plug>(skkeleton-enable)", { silent = true })
     end,
 }
 
